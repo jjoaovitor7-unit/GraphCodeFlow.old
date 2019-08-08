8c4c9b7a3fe954d66c80b659842af824e6fafff8
João Vítor <53799823+jjoaovitor7-unit@users.noreply.github.com>
2019-08-08 18:24:39 -0300

    Create getNumArestas.cpp

---
+++ src/getNumArestas.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//getNumArestas -> pegar número (quantidade) de arestas
+//numar -> número (quantidade) de arestas
+
+int getNumArestas(int numar){
+    std::cout << "Número de Arestas:";
+    std::cin >> numar;
+    return numar;
+}
