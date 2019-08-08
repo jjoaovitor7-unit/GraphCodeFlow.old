20623d893d018c51932889e0caa8ba9261a955ed
João Vítor <53799823+jjoaovitor7-unit@users.noreply.github.com>
2019-08-08 18:18:50 -0300

    Create getNumVertices.cpp

---
+++ src/getNumVertices.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//getNumVertices -> pegar o número (quantidade) de vértices
+//numv -> número (quantidade) de vértices
+
+int getNumVertices(int numv){
+  std::cout << "Número de Vértices:";
+  std::cin >> numv;
+  return numv;
+}
