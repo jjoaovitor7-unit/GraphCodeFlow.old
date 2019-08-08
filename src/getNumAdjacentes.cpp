e3421b627ca302e29181e0530e1628b84d53f713
João Vítor <53799823+jjoaovitor7-unit@users.noreply.github.com>
2019-08-08 18:27:23 -0300

    Create getNumAdjacentes.cpp

---
+++ src/getNumAdjacentes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//getNumAdjacentes -> pegar o número (quantidade) de adjacentes
+//numad -> número (quantidade) de adjacentes
+
+int getNumAdjacentes(int numad){
+    std::cout << "Número de Adjacentes:";
+    std::cin >> numad;
+    return numad;
+}
