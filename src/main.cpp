1765c477fdd98579795a209480c644786bf4cf48
João Vítor S. F <53799823+jjoaovitor7-unit@users.noreply.github.com>
2019-08-09 13:12:40 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 
+//pedir o número (quantidade) de vértices (texto)
+void requestNumVertices(){
+    std::cout << "Número de Vértices:";
+}
+
+//pedir o número (quantidade) de arestas (texto)
+void requestNumArestas(){
+    std::cout << "Número de Arestas:";
+}
+
+//pedir o número (quantidade) de adjacentes (texto)
+void requestNumAdjacentes(){
+    std::cout << "Número de Adjacentes:";
+}
+
+//pegar o número (quantidade) de vértices
+int getNumVertices(int numv){
+    return numv;
+}
+
+//pegar o número (quantidade) de arestas
+int getNumArestas(int numar){
+    return numar;
+}
+
+//pegar o número (quantidade) de adjacentes
+int getNumAdjacentes(int numad){
+    return numad;
+}
+
 int main(){
-    getNumVertices();
-    getNumArestas();
-    getNumAdjacentes();
+    requestNumVertices();
+    int numav;
+    std::cin >> numv;
+    getNumVertices(numv);
+
+    requestNumArestas();
+    int numar;
+    std::cin >> numar;
+    getNumArestas(numar);
+
+    requestNumAdjacentes();
+    int numad;
+    std::cin >> numad;
+    getNumAdjacentes(numad);
 
     return 0;
 }
