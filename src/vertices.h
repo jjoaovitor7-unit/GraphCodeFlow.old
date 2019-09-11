e5fcdac05b0ab0a9e607d44ebf4b507797cf181f
João Vítor S. F <53799823+jjoaovitor7-unit@users.noreply.github.com>
2019-09-11 16:42:20 -0300

    Create vertices.h

---
+++ src/vertices.h
@@ -0,0 +1,15 @@
+#ifndef VERTICES_H
+#define VERTICES_H
+
+//pedir o número (quantidade) de vértices (texto)
+void requestNumVertices(){
+    std::cout << "Número de Vértices:";
+}
+
+
+//retorna o número (quantidade) de vértices
+int getNumVertices(int numv){
+    return numv;
+}
+
+#endif
