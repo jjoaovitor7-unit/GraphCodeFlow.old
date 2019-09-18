cb82d14a71644fc67d2fe9978d373a7adebcc515
João Vítor S. F <53799823+jjoaovitor7-unit@users.noreply.github.com>
2019-09-18 13:59:10 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,21 @@
 void getAdjacentes(){
     //std::setlocale(LC_ALL, "Portuguese");
     requestNumVertices();
-
-    //pegar o número (quantidade) de vértices e colocar na variável numv
-    int numv;
-    std::cin >> numv;
-    getNumVertices(numv);
+    setNumVertices();
 
     //registro das adjacências da matriz de adjacência
     int nv[numv][numv];
-    for(int i=0; i < getNumVertices(numv); i++){
-      for(int j=0; j < getNumVertices(numv); j++){
+    for(int i=0; i < getNumVertices(); i++){
+      for(int j=0; j < getNumVertices(); j++){
         std::cout << "Adjacência(v" << i << "v" << j << ")(0:não|1:sim):";
         std::cin >> nv[i][j];
       }
     }
 
     //printando a matriz de adjacência
-    for(int i=0; i < getNumVertices(numv); i++){
+    for(int i=0; i < getNumVertices(); i++){
       std::cout << "[";
-      for(int j=0; j < getNumVertices(numv); j++){
+      for(int j=0; j < getNumVertices(); j++){
         std::cout << " ";
         std::cout << nv[i][j];
         std::cout << " ";
@@ -38,9 +34,9 @@ void getAdjacentes(){
     //inserindo a matriz de adjacência em um arquivo(matriz_adjacência.txt)
     std::ofstream matrizAdjFILE;
     matrizAdjFILE.open("matriz_adjacência.txt", std::ios::app);
-    for(int i=0; i < getNumVertices(numv); i++){
+    for(int i=0; i < getNumVertices(); i++){
       matrizAdjFILE << "[";
-      for(int j=0; j < getNumVertices(numv); j++){
+      for(int j=0; j < getNumVertices(); j++){
         matrizAdjFILE << " ";
         matrizAdjFILE << nv[i][j];
         matrizAdjFILE << " ";
@@ -53,8 +49,8 @@ void getAdjacentes(){
 
     //método ehCompleto e ehRegular
     bool ehCompleto=true;
-    for(int i=0; i < getNumVertices(numv); i++) {
-      for(int j=0; j < getNumVertices(numv); j++){
+    for(int i=0; i < getNumVertices(); i++) {
+      for(int j=0; j < getNumVertices(); j++){
         if(nv[i][j] == 0){
           ehCompleto=false;
           break;
@@ -81,10 +77,10 @@ void getAdjacentes(){
     char ehConexo;
     std::cout << "Conexo(S/N):";
     std::cin >> ehConexo;
-    
+
     std::queue<int> fila;
-    for(int i=0; i < getNumVertices(numv); i++){
-      for(int j=0; j < getNumVertices(numv); j++){
+    for(int i=0; i < getNumVertices(); i++){
+      for(int j=0; j < getNumVertices(); j++){
         fila.push(nv[i][j]);
       }
     }
@@ -94,6 +90,7 @@ void getAdjacentes(){
       fila.pop();
     }
     std::cout << std::endl;
+    //se a fila estiver vazia
     if(fila.empty()){
       std::cout << "A fila está vazia!" << std::endl;
     }
